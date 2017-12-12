990e97c041acce30f8f71981203ae5d7fd2e327a
Jacob <jm876913@ohio.edu>
2017-12-12 11:37:23 -0500

    taskB.cpp added but doesn't work yet

---
+++ taskB.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int countLine(char* pName);
+
+int countChar(char* pName);
+
+
+int main(int argc, char** argv){
+	char*pName;	
+	
+	ifstream fin("example.txt");	
+	
+	if(argv == NULL){
+		countLine("Ohio Univerity");
+		countChar("Athens");
+	}
+	
+	
+	if(fin.is_open()){
+		countLine(pName);
+		countChar(pName);
+	}
+
+	return 0;
+}
+
+int countLine(char* pName){	
+	int num_lines = 0;
+	ifstream myfile("example.txt");
+	
+	while(getline(myfile, pName)){
+		++num_lines= 0;
+	}
+	cout << num_lines << "lines" << endl;
+
+}
+
+int countChar(char* pName){
+	ifstream fin("example.txt");	
+	fin>>pName;
+	cout << strlen(pName) << "Characters" << endl;
+	
+}
